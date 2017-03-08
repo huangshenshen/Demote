aa9d2e0a324a0593b7218a8f5cfc18e4f446fda7
joe <935237604@qq.com>
2017-03-08 18:33:17 +0800

    沉浸式状态栏的以及全屏显示

--- statedemo/src/main/java/cm/project/moli/statedemo/MainActivity.java
+++ statedemo/src/main/java/cm/project/moli/statedemo/MainActivity.java
@@ -1,13 +1,53 @@
 package cm.project.moli.statedemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //FullS();
+        chenjin();
+
         setContentView(R.layout.activity_main);
+        //状态栏显示文字 半透明
+        //StatusUtils.compat(this, getResources().getColor(R.color.colorAccent));
+
     }
+    //隐藏状态栏标题栏全屏显示
+    private void FullS(){
+
+        //定义全屏参数
+        int flag= WindowManager.LayoutParams.FLAG_FULLSCREEN;
+        //获得当前窗体对象
+        Window window=MainActivity.this.getWindow();
+        //设置当前窗体为全屏显示
+        window.setFlags(flag, flag);
+    }
+    //全透明
+    private void chenjin(){
+        getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
+                    | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+            window.setNavigationBarColor(Color.TRANSPARENT);
+        }
+
+    }
+
+
+
 }
