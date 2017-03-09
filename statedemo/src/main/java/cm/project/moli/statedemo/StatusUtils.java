4f90ace130bfe97fb3077b729ee81e43f1ae5782
joe <935237604@qq.com>
2017-03-09 15:42:14 +0800

    沉浸式状态栏的以及全屏显示

--- statedemo/src/main/java/cm/project/moli/statedemo/StatusUtils.java
+++ statedemo/src/main/java/cm/project/moli/statedemo/StatusUtils.java
@@ -15,7 +15,7 @@ import android.view.ViewGroup;
 public class StatusUtils {
     private static final int INVALID_VAL = -1;
     private static final int COLOR_DEFAULT = Color.parseColor("#20000000");
-
+//
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     public static void compat(Activity activity, int statusColor)
     {
