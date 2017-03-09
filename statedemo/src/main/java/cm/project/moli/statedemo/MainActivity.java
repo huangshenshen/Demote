0db17353eae84a314832a363f014a30ef1ff7d76
joe <935237604@qq.com>
2017-03-09 15:42:43 +0800

    沉浸式状态栏的以及全屏显示

--- statedemo/src/main/java/cm/project/moli/statedemo/MainActivity.java
+++ statedemo/src/main/java/cm/project/moli/statedemo/MainActivity.java
@@ -9,7 +9,7 @@ import android.view.Window;
 import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
-
+    //
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
